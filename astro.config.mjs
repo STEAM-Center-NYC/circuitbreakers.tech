f64ebdb34cf6f657567f79c5bdebd89ce76c5afb
Justin Forlenza <jforlenza@steamcenter.nyc>
2024-02-14 11:43:31 -0500

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
-export default defineConfig({});
+import vue from "@astrojs/vue"
+
+export default defineConfig({
+  integrations: [vue()]
+})
