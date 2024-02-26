15023c8da86fee93a1a19cefb8cce4ebb8def7d0
Justin <justinforlenza@users.noreply.github.com>
2024-02-26 15:31:24 +0000

    switched framework

---
+++ app.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+useHead({
+  title: 'Circuit Breakers',
+})
+</script>
+
+<template>
+  <header>
+    <h1>Circuit Breakers</h1>
+  </header>
+  <nav>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/team">Team</NuxtLink>
+    <NuxtLink to="/contact-us">Contact</NuxtLink>
+    <NuxtLink to="/projects">Our Projects</NuxtLink>
+  </nav>
+  <main><slot></slot></main>
+</template>
+
+<style>
+  nav a {
+    margin-right: 4px;
+  }
+</style>
