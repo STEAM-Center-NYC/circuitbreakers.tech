b06a0f8378423214b93f60f22f64e275014d62f7
AdrianC167 <ac232126110@gmail.com>
2024-02-28 10:45:35 -0500

    displayed the list using vue

---
+++ pages/team_test.vue
@@ -0,0 +1,29 @@
+<script setup>
+const { data } = await useAsyncData('home', () => queryContent('/team').find())
+</script>
+
+<template>
+    <h1>people:</h1>
+
+<div v-for="values in data">
+    <img :src="values.picture">
+    {{ values.name }}
+    {{ values.websites }}
+    {{ values.linkedin }}
+    {{ values.description }}
+</div>
+  <pre>
+
+  </pre>
+</template>
+
+<style scoped>
+h1 {
+    margin-top: 72px;
+}
+
+div{
+padding-bottom:15px;
+
+}
+</style>
