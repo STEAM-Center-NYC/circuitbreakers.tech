60edc8b6bf2d616ba0de5ccf96a907a5fb7ca2c7
Laweens <laweensfrancois@gmail.com>
2024-02-28 10:09:18 -0500

    pic added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,21 @@ export default defineNuxtConfig({
     prerender: {
       autoSubfolderIndex: false
     }
+  },
+  modules: [
+    '@nuxt/content'
+  ],
+  content: {
+    // ... options
   }
+  
 })
+
+// Create a query looking for anything in content/ directory
+const contentQuery = queryContent()
+
+// Create a query looking into content/articles directory
+const contentQuery = queryContent('articles')
+
+// Create a query looking into content/articles/nuxt3 directory
+const contentQuery = queryContent('articles', 'nuxt3')
