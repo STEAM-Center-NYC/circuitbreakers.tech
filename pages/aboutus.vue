1ac4da59a179fa281be07026e7bdc99efc889f7e
chr1ssy44 <cbeckford2@brooklynsteamcenter.org>
2024-02-29 10:56:46 -0500

    christina

--- pages/aboutus.vue
+++ pages/aboutus.vue
@@ -52,15 +52,12 @@
                 <p>Clients</p>
               </div>
               <div class="col-lg-3 col-6 text-center">
+                <p></p>
+              </div>
+              <div class="col-lg-3 col-6 text-center"></div>
+
+              <div class="col-lg-3 col-6 text-center">
                 <a><NuxtLink class="active" to="/projects">Projects</NuxtLink></a>
-              </div>
-              <div class="col-lg-3 col-6 text-center">
-                <span data-purecounter-start="0" data-purecounter-end="1463" data-purecounter-duration="1" class="purecounter"></span>
-                <p>Hours Of Support</p>
-              </div>
-              <div class="col-lg-3 col-6 text-center">
-                <span data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="1" class="purecounter"></span>
-                <p>Hard Workers</p>
               </div>
             </div>
           </div>
