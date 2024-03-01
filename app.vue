93cd4e86ad62ddc9ebd19c13d104d0189b58b26a
amiril2 <alayne@brooklynsteamcenter.org>
2024-03-01 09:16:48 -0500

    UPDATE: Final Changes

--- app.vue
+++ app.vue
@@ -49,11 +49,11 @@
       <div class="col-lg-3 col-md-6 footer-links">
         <h4>Our Services</h4>
         <ul>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Project Management</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a to="">Web Design</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a to="">Web Development</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a to="">Project Management</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a to="">Marketing</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a to="">Graphic Design</a></li>
         </ul>
       </div>
 
