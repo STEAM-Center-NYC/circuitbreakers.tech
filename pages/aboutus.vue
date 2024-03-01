5ad67ee5414f5af52a1355e895e1c03457a2a660
Mekhi Mcfowler <mmcfowler@brooklynsteamcenter.org>
2024-03-01 09:09:34 -0500

    fixed carousel image change interval

--- pages/aboutus.vue
+++ pages/aboutus.vue
@@ -19,7 +19,8 @@
       <section class="about" data-aos="fade-up">
         <div class="container">
           <div class="row">
-            <div id="carouselExampleSlidesOnly" class="carousel slide col-lg-6" data-bs-ride="carousel">
+            <div id="carouselExampleSlidesOnly" class="carousel slide col-lg-6" data-bs-ride="carousel"
+              data-bs-interval="1000">
               <div class="carousel-inner">
                 <div v-for="index in 10" :key="index" :class="{ 'carousel-item': true, 'active': index === 1 }">
                   <img :src="images[index - 1]" class="d-block w-100 about-img-fluid" :alt="'Image ' + index">
@@ -29,32 +30,35 @@
             <div class="col-lg-6 pt-4 pt-lg-0">
               <h3>Circuit Breakers</h3>
               <p class="fst-italic">
-                Greetings, this is Circuit Breakers! We are seniors presently enrolled at the Brooklyn STEAM Center from various residential high schools such as:
+                Greetings, this is Circuit Breakers! We are seniors presently enrolled at the Brooklyn STEAM Center from
+                various residential high schools such as:
               </p>
               <ul>
                 <li><i class="bi bi-check2-circle"></i> The High School For Global Citizenship</li>
                 <li><i class="bi bi-check2-circle"></i> Medgar Evers College Preparatory School</li>
-                  <li><i class="bi bi-check2-circle"></i> Benjamin Banneker Academy</li>
-                  <li><i class="bi bi-check2-circle"></i> Science Skills Center High School</li>
-                  <li><i class="bi bi-check2-circle"></i> S.T.A.R Early College High School</li>
-                  <li><i class="bi bi-check2-circle"></i> Boys And Girls High School</li>
-                  <li><i class="bi bi-check2-circle"></i>George Westinghouse Career And Technical High School </li>
+                <li><i class="bi bi-check2-circle"></i> Benjamin Banneker Academy</li>
+                <li><i class="bi bi-check2-circle"></i> Science Skills Center High School</li>
+                <li><i class="bi bi-check2-circle"></i> S.T.A.R Early College High School</li>
+                <li><i class="bi bi-check2-circle"></i> Boys And Girls High School</li>
+                <li><i class="bi bi-check2-circle"></i>George Westinghouse Career And Technical High School </li>
               </ul>
               <p>
-                All of us Circuit Breakers have been busy writing our own projects! From constructing stopwatches and ToDo apps to making our own websites!
+                All of us Circuit Breakers have been busy writing our own projects! From constructing stopwatches and ToDo
+                apps to making our own websites!
               </p>
             </div>
           </div>
         </div>
       </section><!-- End About Us Section -->
-      
+
 
       <!-- Facts Section -->
       <section class="facts section-bg" data-aos="fade-up">
         <div class="container">
           <div class="row counters">
             <div class="col-lg-3 col-6 text-center">
-              <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
+              <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1"
+                class="purecounter"></span>
               <p>Clients</p>
             </div>
             <div class="col-lg-3 col-6 text-center">
@@ -63,11 +67,13 @@
             <div class="col-lg-3 col-6 text-center"></div>
 
             <div class="col-lg-3 col-6 text-center">
-              <a><NuxtLink class="active" to="/projects">Projects</NuxtLink></a>
+              <a>
+                <NuxtLink class="active" to="/projects">Projects</NuxtLink>
+              </a>
             </div>
           </div>
         </div>
-        </section>
+      </section>
 
 
       <!-- Our Skills Section -->
@@ -79,32 +85,32 @@
           </div>
           <div class="skills-content">
             <div class="progress">
-              <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"
-                style="width: 100%;">
-                  <span class="skill">HTML</span>
+              <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="100" aria-valuemin="0"
+                aria-valuemax="100" style="width: 100%;">
+                <span class="skill">HTML</span>
               </div>
             </div>
             <div class="progress">
-              <div class="progress-bar bg-dark" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"
-                style="width: 100%;">
+              <div class="progress-bar bg-dark" role="progressbar" aria-valuenow="90" aria-valuemin="0"
+                aria-valuemax="100" style="width: 100%;">
                 <span class="skill">CSS </span>
               </div>
             </div>
             <div class="progress">
-              <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"
-                style="width: 100%;">
+              <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="75" aria-valuemin="0"
+                aria-valuemax="100" style="width: 100%;">
                 <span class="skill">JavaScript </span>
               </div>
             </div>
             <div class="progress">
-              <div class="progress-bar bg-success" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"
-                style="width: 100%;">
+              <div class="progress-bar bg-success" role="progressbar" aria-valuenow="55" aria-valuemin="0"
+                aria-valuemax="100" style="width: 100%;">
                 <span class="skill">Vue </span>
               </div>
             </div>
             <div class="progress">
-              <div class="progress-bar bg-info" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"
-                style="width: 100%;">
+              <div class="progress-bar bg-info" role="progressbar" aria-valuenow="30" aria-valuemin="0"
+                aria-valuemax="100" style="width: 100%;">
                 <span class="skill">Python/NB </span>
               </div>
             </div>
@@ -136,8 +142,6 @@ export default {
 };
 </script>
 
-<style scoped>
-.carousel-item {
+<style scoped>.carousel-item {
   transition: transform 1.5s ease-in-out;
-}
-</style>
+}</style>
